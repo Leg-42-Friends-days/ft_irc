b05a243e43baebaba474014cb03031ca103b596e
mickzhan <mickzhan@student.42.fr>
2026-09-10 16:08:13 +0200

    port gestion alpha

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 
 bool checkPort(std::string port)
 {
+    for (size_t i = 0; i < port.size(); i++)
+    {
+        if (!isdigit(port[i]))
+            return (true);
+    }
+
     int portValue = std::atoi(port.c_str());
 
     if (portValue < 0 || portValue > 65535)
@@ -15,13 +21,17 @@ int main(int ac, char **av)
     if (ac != 3)
     {
         std::cout << "execute : ./ircserv <port> <password>" << std::endl;
-        return (1);
+        return EXIT_FAILURE;
     }
+
     std::string port = av[1];
+    std::string password = av[2];
+
     if (checkPort(av[1]))
     {
         std::cout << "Error : invalid port!" << std::endl;
-        return (1);
+        return EXIT_FAILURE;
     }
+
     return EXIT_SUCCESS;
 }
